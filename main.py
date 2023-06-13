28fd6fda4c51f67e8d5d57f3e0c2c4172e75d363
Charles Yakubu <127857110+Charlexzy@users.noreply.github.com>
2023-06-13 23:12:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI, File, UploadFile
-from PIL import Image
-from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn  # ASGI
 import numpy as np
+from io import BytesIO
+from PIL import Image
 import tensorflow as tf
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+MODEL = tf.keras.models.load_model("../potatoes.h5", custom_objects={'tf': tf})
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
 # Define the same data augmentation parameters as during training
 data_augmentation = tf.keras.preprocessing.image.ImageDataGenerator(
@@ -15,21 +36,6 @@ data_augmentation = tf.keras.preprocessing.image.ImageDataGenerator(
     horizontal_flip=True
 )
 
-app = FastAPI()
-
-MODEL_PATH = "gs://potatodiseaseclassifier/models/potatoes.h5"
-
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
-
-# Load the model
-MODEL = None
-
-def load_model():
-    global MODEL
-    if MODEL is None:
-        MODEL = tf.keras.models.load_model(MODEL_PATH)
-
-# Preprocess the image
 def preprocess_image(image):
     # Resize the image to the desired dimensions
     image = image.resize((256, 256))
@@ -37,24 +43,23 @@ def preprocess_image(image):
     img_array = tf.keras.preprocessing.image.img_to_array(image)
     # Expand the dimensions of the image to match the expected input shape
     img_array = np.expand_dims(img_array, axis=0)
-    # Apply data normalization
-    img_array = img_array / 255.0
+    # Apply data augmentation and normalization
+    img_array = data_augmentation.flow(img_array, shuffle=False).next()
     return img_array
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    # Load the model if not loaded already
-    load_model()
-
+async def predict(
+    file: UploadFile = File(...)
+):
     # Read the uploaded file as an image
     image = Image.open(BytesIO(await file.read()))
-
+    
     # Preprocess the image
     img_array = preprocess_image(image)
-
+    
     # Make predictions by calling the loaded model on the preprocessed image
     predictions = MODEL.predict(img_array)
-
+    
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = float(np.max(predictions[0]))
 
@@ -62,4 +67,6 @@ async def predict(file: UploadFile = File(...)):
         'class': predicted_class,
         'confidence': confidence
     }
-    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
